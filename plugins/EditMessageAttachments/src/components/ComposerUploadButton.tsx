84058dac99b262a2fd4e36f868b5b86874f85823
Federico <38290480+FedeIlLeone@users.noreply.github.com>
2024-11-23 17:53:21 +0100

    fix: types and cleanup

--- plugins/EditMessageAttachments/src/components/ComposerUploadButton.tsx
+++ plugins/EditMessageAttachments/src/components/ComposerUploadButton.tsx
@@ -3,6 +3,7 @@ import ImagePlusIcon from "@common/components/icons/ImagePlusIcon";
 import ChannelStore from "@common/stores/ChannelStore";
 import UploadAttachmentStore, { DraftType } from "@common/stores/UploadAttachmentStore";
 import UploaderUtils from "@common/utils/UploaderUtils";
+import classNames from "classnames";
 import { common, components } from "replugged";
 import { logger } from "..";
 import { MAX_UPLOAD_COUNT } from "../constants";
@@ -25,7 +26,7 @@ interface ComposerUploadButtonProps {
 export default (props: ComposerUploadButtonProps): React.ReactElement | null => {
   const { attachmentsCount, channelId } = props;
 
-  const ref = React.useRef<(FileInputType & Element) | null>(null);
+  const ref = React.useRef<FileInputType | null>(null);
 
   const uploadsCount = Flux.useStateFromStores([UploadAttachmentStore], () =>
     UploadAttachmentStore.getUploadCount(channelId, DraftType.EditedChannelMessage),
@@ -36,104 +37,95 @@ export default (props: ComposerUploadButtonProps): React.ReactElement | null => 
     () => ChannelStore.getChannel(channelId),
     [channelId],
   );
-  const [dragHover, setDragHover] = React.useState(false);
-  const dndRef = React.useRef<HTMLSpanElement | null>(null);
-  const dragover = React.useCallback(() => {
-    setDragHover(true);
-  }, []);
-  const dragleave = React.useCallback(() => {
-    setDragHover(false);
-  }, []);
-  const drop = React.useCallback((event: React.DragEvent<HTMLSpanElement>) => {
-    setDragHover(false);
-    if (!channel) return;
-    const count = event?.dataTransfer?.files?.length ?? 0;
-    if (attachmentsCount + count + uploadsCount > MAX_UPLOAD_COUNT) {
-      modal.alert({
-        title: intl.string(discordT.ATTACHMENT_TOO_MANY_ERROR_TITLE),
-        body: intl.formatToPlainString(discordT.ATTACHMENT_TOO_MANY_ERROR_MESSAGE, {
-          limit: MAX_UPLOAD_COUNT,
-        }),
-      });
-      return;
-    }
-
-    UploaderUtils.promptToUpload(
-      event?.dataTransfer?.files,
-      channel,
-      DraftType.EditedChannelMessage,
-      {
-        requireConfirm: true,
-        showLargeMessageDialog: false,
-      },
-    );
-  }, []);
-
-  React.useEffect(() => {
-    if (!dndRef.current) return;
-    dndRef.current.addEventListener("dragover", dragover, false);
-    dndRef.current.addEventListener("dragleave", dragleave, false);
-    dndRef.current.addEventListener("drop", drop, false);
-    return () => {
-      if (!dndRef.current) return;
-      dndRef.current.removeEventListener("dragover", dragover, false);
-      dndRef.current.removeEventListener("dragleave", dragleave, false);
-      dndRef.current.removeEventListener("drop", drop, false);
-    };
-  }, []);
 
   if (!channel) {
     logger.error("Channel is null", channelId);
     return null;
   }
 
+  const handleUpload = React.useCallback(
+    (files: FileList | null) => {
+      const count = files?.length ?? 0;
+      if (attachmentsCount + count + uploadsCount > MAX_UPLOAD_COUNT) {
+        modal.alert({
+          title: intl.string(discordT.ATTACHMENT_TOO_MANY_ERROR_TITLE),
+          body: intl.formatToPlainString(discordT.ATTACHMENT_TOO_MANY_ERROR_MESSAGE, {
+            limit: MAX_UPLOAD_COUNT,
+          }),
+        });
+        return;
+      }
+
+      UploaderUtils.promptToUpload(files, channel, DraftType.EditedChannelMessage, {
+        requireConfirm: true,
+        showLargeMessageDialog: false,
+      });
+    },
+    [attachmentsCount, channel, uploadsCount],
+  );
+
+  const [dragHover, setDragHover] = React.useState(false);
+  const dndRef = React.useRef<HTMLDivElement | null>(null);
+
+  const handleDragOver = (): void => {
+    setDragHover(true);
+  };
+
+  const handleDragLeave = (): void => {
+    setDragHover(false);
+  };
+
+  const handleDrop = React.useCallback(
+    (event: DragEvent) => {
+      handleDragLeave();
+      if (event.dataTransfer) handleUpload(event.dataTransfer.files);
+    },
+    [handleUpload],
+  );
+
+  React.useEffect(() => {
+    const { current } = dndRef;
+    if (!current) return;
+
+    current.addEventListener("dragover", handleDragOver, false);
+    current.addEventListener("dragleave", handleDragLeave, false);
+    current.addEventListener("drop", handleDrop, false);
+
+    return () => {
+      const { current } = dndRef;
+      if (!current) return;
+
+      current.removeEventListener("dragover", handleDragOver, false);
+      current.removeEventListener("dragleave", handleDragLeave, false);
+      current.removeEventListener("drop", handleDrop, false);
+    };
+  }, [handleDrop]);
+
   return (
-    <span ref={dndRef} className={dragHover ? "editMessageAttachments-uploadInput-dragHover" : ""}>
-      <Clickable
-        className="editMessageAttachments-uploadInput"
-        onKeyDown={(event) => {
-          if (event.key === "Enter") {
-            if (ref.current) ref.current.activateUploadDialogue();
-          }
-        }}>
-        <FileInput
-          ref={ref}
-          onChange={(event) => {
-            const count = event.currentTarget.files?.length ?? 0;
-            if (attachmentsCount + count + uploadsCount > MAX_UPLOAD_COUNT) {
-              modal.alert({
-                title: intl.string(discordT.ATTACHMENT_TOO_MANY_ERROR_TITLE),
-                body: intl.formatToPlainString(discordT.ATTACHMENT_TOO_MANY_ERROR_MESSAGE, {
-                  limit: MAX_UPLOAD_COUNT,
-                }),
-              });
-              return;
-            }
-
-            UploaderUtils.promptToUpload(
-              event.currentTarget.files,
-              channel,
-              DraftType.EditedChannelMessage,
-              {
-                requireConfirm: true,
-                showLargeMessageDialog: false,
-              },
-            );
-
-            event.currentTarget.value = "";
-          }}
-          disabled={attachmentsCount + uploadsCount >= MAX_UPLOAD_COUNT}
-          multiple={channel.rateLimitPerUser <= 0}
-          tabIndex={-1}
-          aria-hidden
-        />
-        <ImagePlusIcon
-          className="editMessageAttachments-uploadIcon"
-          color="currentColor"
-          width={28}
-          height={28}
-        />
-      </Clickable>
-    </span>
+    <Clickable
+      className={classNames("editMessageAttachments-uploadInput", dragHover && "dragging")}
+      onKeyDown={(event) => {
+        if (event.key === "Enter") {
+          if (ref.current) ref.current.activateUploadDialogue();
+        }
+      }}>
+      <FileInput
+        ref={ref}
+        onChange={(event) => {
+          handleUpload(event.currentTarget.files);
+          event.currentTarget.value = "";
+        }}
+        disabled={attachmentsCount + uploadsCount >= MAX_UPLOAD_COUNT}
+        multiple={channel.rateLimitPerUser <= 0}
+        tabIndex={-1}
+        aria-hidden
+      />
+      <ImagePlusIcon
+        className="editMessageAttachments-uploadIcon"
+        color="currentColor"
+        width={28}
+        height={28}
+      />
+    </Clickable>
   );
 };
